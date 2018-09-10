59589916c6ebb92e2bda4e2a4124d5a44ed4c256
tkmru <i.am.tkmru@gmail.com>
2018-09-10 15:07:36 +0000

    fix unicorn

--- unicorn_config.rb
+++ unicorn_config.rb
@@ -1,2 +1,6 @@
 worker_processes 10
 preload_app true
+listen "/tmp/unicorn.sock", :backlog => 2048
+pid "/home/isucon/webapp/ruby/unicorn.pid"
+stderr_path "/tmp/unicorn-err.log"
+stdout_path "/tmp/unicorn-out.log"
